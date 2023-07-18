6e924ea079f2db02218e7e49d88be14e9a0eff87
ricardodebonaroque@gmail.com <ricardodebonaroque@gmail.com>
2023-07-18 12:19:28 +0000

    Criado as staging productcategory e productsubcategory Tudo rodando normal

---
+++ models/staging/sap_adventureworks/stg_sap__productcategory.sql
@@ -0,0 +1,11 @@
+with 
+    fonte_product_category as (
+        select 
+            cast(productcategoryid as int) as product_category_id						
+            , cast(name	as string) as name_product					
+            , cast(rowguid as string) as row_guid					 	
+            , cast(modifieddate as string) as modified_date
+        from {{ source('sap', 'productcategory') }}
+    )
+select*
+from fonte_product_category

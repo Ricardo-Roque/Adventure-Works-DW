b37ff6bed95a458970259b699ed56d835598be89
ricardodebonaroque@gmail.com <ricardodebonaroque@gmail.com>
2023-07-18 02:00:39 +0000

    Criado as pastas marts e staging. Criado a stg_sap__product.

---
+++ models/staging/erp_adventureworks/stg_sap__product.sql
@@ -0,0 +1,34 @@
+with 
+    fonte_product as (
+        select
+            cast(productid as int) as id_product						
+            , cast(name as string) as name_product						
+            , cast(productnumber as string) as number_product						
+            , cast(makeflag as string) as make_flag						
+            --, cast(finishedgoodsflag						
+            --, cast(color						
+            , cast(safetystocklevel as int) as safety_stock_level						
+            , cast(reorderpoint as int) as re_order_point 						
+            , cast(standardcost as numeric) as standard_cost						
+            , cast(listprice as numeric) as list_price						
+            --, cast(size					
+            --, cast(sizeunitmeasurecode						
+            --, cast(weightunitmeasurecode						
+            --, cast(weight						
+            , cast(daystomanufacture as int) as days_to_manufacture						
+            , cast(productline as string) as product_line					
+            , cast(class as string) as class_product						
+            , cast(style as string) as style_product 						
+            , cast(productsubcategoryid as int) as product_subcategory_id 						
+            , cast(productmodelid as int) as product_model_id						
+            , cast(sellstartdate as string) as sell_start_date						
+            , cast(sellenddate as string) as sell_end_date						
+            , cast(discontinueddate as int) as discontinued_date						
+            , cast(rowguid as string) as row_guid						
+            , cast(modifieddate as string) as modified_date
+
+        from {{ source('sap', 'product') }}
+    )
+
+select*
+from fonte_product

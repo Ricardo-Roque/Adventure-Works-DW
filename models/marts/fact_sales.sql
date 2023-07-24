fec298201a4dfc389fcbb72349fd5ec2522a8f75
ricardodebonaroque@gmail.com <ricardodebonaroque@gmail.com>
2023-07-24 23:44:53 +0000

    Tentando criar ainda a fact_sales, não estou conseguindo por enquanto modelar ela certinho.

---
+++ models/marts/fact_sales.sql
@@ -0,0 +1,68 @@
+with 
+    sales as (
+        select *
+        from {{ ref('int_sales_orders') }}
+    )
+    , address as (
+        select *
+        from {{ ref('dim_address') }}
+    )
+    , credit_card as (
+        select *
+        from {{ ref('dim_creditcard') }}
+    )
+    , product as (
+        select *
+        from {{ ref('dim_product') }}
+    )
+    , sales_reason as (
+        select *
+        from {{ ref('dim_salesreason') }}
+    )
+    , join_tabelas as (
+        select 
+            sales.sales_order_id						
+            , sales.sales_oder_detail_id	
+            , sales.id_product	
+            , sales.customer_id									
+            , sales.ship_to_address_id						
+            , sales.credit_card_id	
+            , sales.unit_price	
+            , sales.order_qty		
+            , sales.unit_price_discount												
+            , sales.order_date						
+            , sales.due_date						
+            , sales.ship_date						
+            , sales.status 										
+            , sales.sub_total						
+            , sales.tax_amt						
+            , sales.freight						
+            , sales.total_due   
+            , address.state_province_id						
+            , address.addres_line_1 					
+            , address.address_line_2
+            , address.state_name 
+            , address.city		
+            , address.country_region_name							
+            , credit_card.card_type	
+            , product.product_name
+            , product.name_product_category
+            , product.name_product_subcategory
+            , sales_reason.sales_reason						
+            , sales_reason.reason_type
+        from sales
+        left join address on sales.ship_to_address_id = address.id_address
+        left join credit_card on sales.credit_card_id = credit_card.credit_card_id
+        left join product on sales.id_product = product.id_product
+        left join sales_reason on sales.sales_order_id = sales_reason.sales_order_id
+    )
+    , transformation as (
+        select 
+            * 
+            , (total_due / order_qty) as gross_total
+            , (sub_total / order_qty) as net_total
+            , freight / (count(*) over(partition by sales_oder_detail_id)) as freight_per_items
+        from join_tabelas
+    )
+select * 
+from transformation
--- models/marts/intermediate/int_sales_orders.sql
+++ models/marts/intermediate/int_sales_orders.sql
@@ -12,35 +12,41 @@ with
             sales_order_detail.sales_order_id						
             , sales_order_detail.sales_oder_detail_id	
             , sales_order_detail.id_product	
-            , sales_order_header.customer_id						
-            , sales_order_header.sales_person_id					
-            , sales_order_header.territory_id						
-            , sales_order_header.bill_to_address_id						
+            , sales_order_header.customer_id									
             , sales_order_header.ship_to_address_id						
-            , sales_order_header.ship_method_id						
             , sales_order_header.credit_card_id	
-            , sales_order_detail.special_offer_id	
-            , sales_order_header.currency_rate_id	
             , sales_order_detail.unit_price	
             , sales_order_detail.order_qty		
-            , sales_order_detail.unit_price_discount											
-            , sales_order_header.revision_number						
+            , sales_order_detail.unit_price_discount												
             , sales_order_header.order_date						
             , sales_order_header.due_date						
             , sales_order_header.ship_date						
-            , sales_order_header.status 						
-            , sales_order_header.purchase_order_number						
-            , sales_order_header.account_number 						
-            , sales_order_header.credit_card_approval_code						
+            , sales_order_header.status 										
             , sales_order_header.sub_total						
             , sales_order_header.tax_amt						
             , sales_order_header.freight						
             , sales_order_header.total_due
-            , sales_order_detail.carrier_tracking_number
             , sales_order_detail.row_guid					
             , sales_order_detail.modified_date
+            --, sales_order_detail.carrier_tracking_number
+            --, sales_order_header.credit_card_approval_code	
+            --, sales_order_header.account_number 						
+            --, sales_order_header.purchase_order_number	
+            --, sales_order_header.revision_number		
+            --, sales_order_detail.special_offer_id	
+            --, sales_order_header.currency_rate_id
+            --, sales_order_header.ship_method_id	
+            --, sales_order_header.bill_to_address_id	
+            --, sales_order_header.sales_person_id		
+            --, sales_order_header.territory_id	
         from sales_order_detail
         left join sales_order_header on sales_order_detail.sales_order_id = sales_order_header.sales_order_id
+    )
+    , sales_key as (
+        select 
+            cast((sales_oder_detail_id || '-' || customer_id) as string) as pk_sales
+            , *
+        from join_sales_order
     )
 select *
 from join_sales_order
